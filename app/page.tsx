76b6009549296a3840ef61092eb8c3baecf8600d
hoangchuong <hoangchuong300494@gmail.com>
2024-12-15 00:44:27 +0700

    add new project clothing-store

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background flex justify-center items-center">
+    <div className="animate-in bg-gray-800 text-white p-6 rounded-lg">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS Animate!</h1>
+      <p>Đây là một ví dụ sử dụng hiệu ứng fade-in.</p>
+    </div>
+  </div>
+  );
+}
